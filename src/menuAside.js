565a145923def346ddf3e7d29c21a0c2e9eb7e08
LeeChangHwan-git <chlee.github@gmail.com>
2025-06-10 13:01:24 +0900

    [add]aside menu 수정

--- src/menuAside.js
+++ src/menuAside.js
@@ -48,50 +48,50 @@ export default [
     label: 'GitHub Repo',
     icon: mdiGithub,
   },
-  {
-    to: '/tables',
-    label: 'Tables',
-    icon: mdiTable,
-  },
-  {
-    to: '/forms',
-    label: 'Forms',
-    icon: mdiSquareEditOutline,
-  },
-  {
-    to: '/ui',
-    label: 'UI',
-    icon: mdiTelevisionGuide,
-  },
-  {
-    to: '/responsive',
-    label: 'Responsive',
-    icon: mdiResponsive,
-  },
-  {
-    to: '/',
-    label: 'Styles',
-    icon: mdiPalette,
-  },
-  {
-    to: '/profile',
-    label: 'Profile',
-    icon: mdiAccountCircle,
-  },
-  {
-    to: '/login',
-    label: 'Login',
-    icon: mdiLock,
-  },
-  {
-    to: '/error',
-    label: 'Error',
-    icon: mdiAlertCircle,
-  },
-  {
-    href: 'https://github.com/justboil/admin-one-react-tailwind',
-    label: 'React version',
-    icon: mdiReact,
-    target: '_blank',
-  },
+  // {
+  //   to: '/tables',
+  //   label: 'Tables',
+  //   icon: mdiTable,
+  // },
+  // {
+  //   to: '/forms',
+  //   label: 'Forms',
+  //   icon: mdiSquareEditOutline,
+  // },
+  // {
+  //   to: '/ui',
+  //   label: 'UI',
+  //   icon: mdiTelevisionGuide,
+  // },
+  // {
+  //   to: '/responsive',
+  //   label: 'Responsive',
+  //   icon: mdiResponsive,
+  // },
+  // {
+  //   to: '/',
+  //   label: 'Styles',
+  //   icon: mdiPalette,
+  // },
+  // {
+  //   to: '/profile',
+  //   label: 'Profile',
+  //   icon: mdiAccountCircle,
+  // },
+  // {
+  //   to: '/login',
+  //   label: 'Login',
+  //   icon: mdiLock,
+  // },
+  // {
+  //   to: '/error',
+  //   label: 'Error',
+  //   icon: mdiAlertCircle,
+  // },
+  // {
+  //   href: 'https://github.com/justboil/admin-one-react-tailwind',
+  //   label: 'React version',
+  //   icon: mdiReact,
+  //   target: '_blank',
+  // },
 ]
